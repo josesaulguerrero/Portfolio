17ee5551cda5e5e850554b6f14a282f13499c758
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2021-10-20 10:28:53 -0500

    styled the Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,9 +7,10 @@ import '../assets/styles/Hero.css';
 export const Hero = () => {
    return (
       <main className="Hero">
-         <h1 className='title--large'>Jose Saul</h1>
-         <h2 className="title--small">& I create amazing experiences.</h2>
-         <p className="text--large">
+         <p className="Hero__greet text--large">Hi, I am</p>
+         <h1 className='Hero__name title--large'>Jose Saul</h1>
+         <h2 className="Hero__features title--small">& I create amazing experiences.</h2>
+         <p className="Hero__introduction text--large">
             I'm a web developer who builds friendly user interfaces using HTML, CSS, JavaScript, and React, following some good practices like reusable, clean, and readable code, which leads to accessible, fast and smooth experiences.
          </p>
          <Button size='medium'>View work</Button>
