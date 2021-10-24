cc93d7aa3222d13877fd322ebf8f5abc92d268a2
Jose Saul Guerrero Serrano <josesaulserrano15@gmail.com>
2021-10-24 12:07:57 -0500

    added meta tags for twitter cards

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,6 +6,8 @@ import { Helmet } from 'react-helmet';
 export const Head = () => {
    return (
       <Helmet>
+         <meta name="description" content="This is a webpage where I talk about me and the projects I have built along my way to become a front-end developer." />
+         {/* open graph */}
          <meta property="og:title" content="Jose Guerrero" />
          <meta property="og:type" content="website" />
          <meta property="og:image" content="https://i.postimg.cc/KvR9kvJ1/Screenshot-from-2021-10-24-09-23-24.png" />
@@ -13,6 +15,11 @@ export const Head = () => {
          <meta property="og:image:type" content="image/png" />
          <meta property="og:url" content="https://joseguerrero.vercel.app" />
          <meta property="og:description" content="This is a webpage where I talk about me and the projects I have built along my way to become a front-end developer." />
+         <meta property="og:site_name" content="Jose Guerrero" />
+         {/* twitter cards */}
+         <meta name="twitter:title" content="Jose Guerrero" />
+         <meta name="twitter:description" content="This is a webpage where I talk about me and the projects I have built along my way to become a front-end developer." />
+         <meta name="twitter:image" content="https://i.postimg.cc/KvR9kvJ1/Screenshot-from-2021-10-24-09-23-24.png" />
       </Helmet>
    );
 };
